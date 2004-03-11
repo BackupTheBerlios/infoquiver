6f3468febd627e4b71f8fa3416750c4db8eb60e5
netseeker <netseeker>
2004-03-11 19:50:53 +0000

    *** empty log message ***

---
+++ InfoQuiver/src/java/net/sf/iquiver/metaformat/impl/DefaultDocument.java
@@ -0,0 +1,33 @@
+/*
+ * Created on 10.03.2004
+ *
+ */
+package net.sf.iquiver.metaformat.impl;
+
+import net.sf.iquiver.metaformat.AbstractDocument;
+
+/**
+ * @author netseeker aka Michael Manske
+ */
+public class DefaultDocument extends AbstractDocument
+{    
+    byte[] rawContent;
+    
+    /* (non-Javadoc)
+     * @see net.sf.iquiver.metaformat.Document#getRawContent()
+     */
+    public byte[] getRawContent()
+    {
+        return this.rawContent;
+    }
+
+    /* (non-Javadoc)
+     * @see net.sf.iquiver.metaformat.Document#clear()
+     */
+    public void clear()
+    {
+        super.clearDocumentMembers();
+        this.rawContent = null;
+    }
+
+}
