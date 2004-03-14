60850276dd44d27fa354fc8c6586886c0e1d7282
netseeker <netseeker>
2004-03-14 13:45:35 +0000

    *** empty log message ***

---
+++ InfoQuiver/src/java/net/sf/iquiver/transport/Transport.java
@@ -0,0 +1,13 @@
+/*
+ * Created on 14.03.2004
+ *
+ */
+package net.sf.iquiver.transport;
+
+/**
+ * @author netseeker aka Michael Manske
+ */
+public interface Transport
+{
+    public String[] getAttributeIdentifiers();
+}
