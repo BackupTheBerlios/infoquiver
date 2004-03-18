2c023fecb80dc81be04a3878dd43b0b5601afe9d
netseeker <netseeker>
2004-03-18 17:05:28 +0000

    *** empty log message ***

--- InfoQuiver/src/java/net/sf/iquiver/transport/impl/HTTPTransport.java
+++ InfoQuiver/src/java/net/sf/iquiver/transport/impl/HTTPTransport.java
@@ -75,7 +75,7 @@ public class HTTPTransport implements Fetcher
             // execute the method.
             client.executeMethod( method);
             String encoding = ((GetMethod)method).getResponseCharSet(); 
-            DefaultDocument doc = new DefaultDocument(method.getResponseBodyAsString());           
+            DefaultDocument doc = new DefaultDocument(method.getResponseBody(), encoding);           
             documents.add(doc);
         }
         catch ( UnsupportedEncodingException ue )
