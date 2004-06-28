0882a8d76082bb7368429eecd1a9cfc2a90fb162
netseeker <netseeker>
2004-06-28 20:48:59 +0000

    simplified configuration

--- InfoQuiver/src/java/net/sf/iquiver/util/om/CacheBackedPeer.java
+++ InfoQuiver/src/java/net/sf/iquiver/util/om/CacheBackedPeer.java
@@ -4,12 +4,10 @@
  */
 package net.sf.iquiver.util.om;
 
-import net.sf.iquiver.configuration.Configurable;
-import net.sf.iquiver.configuration.Configuration;
 import net.sf.swarmcache.CacheConfiguration;
 import net.sf.swarmcache.CacheFactory;
 
-import org.apache.avalon.framework.configuration.ConfigurationException;
+import org.apache.commons.configuration.Configuration;
 import org.apache.commons.lang.ObjectUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -22,56 +20,99 @@ import org.apache.torque.util.BasePeer;
  * provided by this class.
  * @author netseeker aka Michael Manske 
  */
-public class CacheBackedPeer extends BasePeer implements Configurable
+public class CacheBackedPeer extends BasePeer
 {
-    protected static CacheFactory cacheFactory;
-    private static final Log logger = LogFactory.getLog( CacheBackedPeer.class);
+    private static CacheFactory cacheFactory;
+    private static final Log logger = LogFactory.getLog( CacheBackedPeer.class );
+    private static boolean enabledAtStartup = false;
+    private static boolean enableCacheForCriteriaSelects = false;
+    private static String[] excludeEnableAtStartUp;
+    private static String[] excludeEnableCacheForCriteriaSelects;
 
     /*
      * (non-Javadoc)
      * 
      * @see org.apache.avalon.framework.configuration.Configurable#configure(org.apache.avalon.framework.configuration.DefaultProperiesConfiguration)
      */
-    public final void configure( Configuration configuration ) throws ConfigurationException
+    public static final synchronized void configure( Configuration configuration )
     {
-        CacheConfiguration cacheConfiguration = new CacheConfiguration();
-        String cacheType = (String) ObjectUtils.defaultIfNull( configuration.getString( "type"),
-                CacheConfiguration.TYPE_HYBRID);
-        String cacheSize = (String) ObjectUtils.defaultIfNull( configuration.getString( "size"), "10000");
-        String multicastIP = configuration.getString( "multicast.ip");
-        boolean enabledAtStartUp = configuration.getBoolean( "enabledAtStartup");
-        boolean enableCacheForCriteriaSelects = configuration.getBoolean( "enableCacheForCriteriaSelects");
-
-        logger.info( "Initializing cache of type " + cacheType + " with a cache size of " + cacheSize);
-
-        if ( enabledAtStartUp )
+        enabledAtStartup = configuration.getBoolean("enabledAtStartup");
+        enableCacheForCriteriaSelects = configuration.getBoolean( "enableCacheForCriteriaSelects" );
+        excludeEnableAtStartUp = configuration.getStringArray( "excludeEnableAtStartUp" );
+        excludeEnableCacheForCriteriaSelects = configuration.getStringArray( "excludeEnableCacheForCriteriaSelects" );
+              
+        if ( enabledAtStartup )
         {
-            logger.info( "Cache will be activated (enabledAtStartup=true");
-        }
-        else
-        {
-            logger.info( "Cache will NOT be activated (enabledAtStartup=false or not set");
-        }
+            logger.info( "Cache will be activated (enabledAtStartup=true" );
+            
+            CacheConfiguration cacheConfiguration = new CacheConfiguration();
+            String cacheType = (String) ObjectUtils.defaultIfNull( configuration.getString( "type" ),
+                    CacheConfiguration.TYPE_HYBRID );
+            String cacheSize = (String) ObjectUtils.defaultIfNull( configuration.getString( "size" ), "10000" );
+            String multicastIP = configuration.getString( "multicast.ip" );
 
-        if ( enableCacheForCriteriaSelects )
+            logger.info( "Initializing cache of type " + cacheType + " with a cache size of " + cacheSize );            
+            
+            cacheConfiguration.setCacheType( cacheType );
+            cacheConfiguration.setLRUCacheSize( cacheSize );
+            if ( multicastIP != null && multicastIP.length() > 0 )
+            {
+                logger.info( "Setting multicast IP to " + multicastIP );
+                cacheConfiguration.setMulticastIP( multicastIP );
+            }
+            cacheFactory = new CacheFactory( cacheConfiguration );
+            if ( enableCacheForCriteriaSelects )
+            {
+                logger.info( "CacheForCriteriaSelects will be activated" );
+            } else
+            {
+                logger
+                        .info( "CacheForCriteriaSelects will NOT be activated (enableCacheForCriteriaSelects=false or not set" );
+            }
+            logger.info( "Cache initialization completed" );
+        } else
         {
-            logger.info( "CacheForCriteriaSelects will be activated");
+            logger.info( "Cache will NOT be activated (enabledAtStartup=false or not set" );
         }
-        else
-        {
-            logger.info( "CacheForCriteriaSelects will NOT be activated (enableCacheForCriteriaSelects=false or not set");
-        }
-
-        cacheConfiguration.setCacheType( cacheType);
-        cacheConfiguration.setLRUCacheSize( cacheSize);
-        if ( multicastIP != null && multicastIP.length() > 0 )
-        {
-            logger.info( "Setting multicast IP to " + multicastIP);
-            cacheConfiguration.setMulticastIP( multicastIP);
-        }
-
-        cacheFactory = new CacheFactory( cacheConfiguration );
-        logger.info( "Cache initialization completed");
     }
 
+    /**
+     * @return Returns the cacheFactory.
+     */
+    public static CacheFactory getCacheFactory()
+    {
+        return cacheFactory;
+    }
+
+    /**
+     * @return Returns the enableCacheForCriteriaSelects.
+     */
+    public static boolean isEnableCacheForCriteriaSelects()
+    {
+        return enableCacheForCriteriaSelects;
+    }
+
+    /**
+     * @return Returns the enabledAtStartup.
+     */
+    public static boolean isEnabledAtStartup()
+    {
+        return enabledAtStartup;
+    }
+
+    /**
+     * @return Returns the excludeEnableAtStartUp.
+     */
+    public static String[] getExcludeEnableAtStartUp()
+    {
+        return excludeEnableAtStartUp;
+    }
+
+    /**
+     * @return Returns the excludeEnableCacheForCriteriaSelects.
+     */
+    public static String[] getExcludeEnableCacheForCriteriaSelects()
+    {
+        return excludeEnableCacheForCriteriaSelects;
+    }
 }
