9d9f6306fdd74610fe0fe9c2a4573ae33fa40637
netseeker <netseeker>
2004-06-29 20:01:06 +0000

    added support for a complete jgroup channel properties string

--- InfoQuiver/src/java/net/sf/iquiver/util/om/CacheBackedPeer.java
+++ InfoQuiver/src/java/net/sf/iquiver/util/om/CacheBackedPeer.java
@@ -14,11 +14,11 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.torque.util.BasePeer;
 
 /**
- * CacheBackedPeer is the base class of all torque peers used in iquiver.
- * Basically CacheBackedPeer guarantees to configure SwarmCache for each
- * generated torque object type. All subclasses use the net.sf.swarmcache.CacheFactory
+ * CacheBackedPeer is the base class of all torque peers used in iquiver. Basically CacheBackedPeer guarantees to
+ * configure SwarmCache for each generated torque object type. All subclasses use the net.sf.swarmcache.CacheFactory
  * provided by this class.
- * @author netseeker aka Michael Manske 
+ * 
+ * @author netseeker aka Michael Manske
  */
 public class CacheBackedPeer extends BasePeer
 {
@@ -36,41 +36,49 @@ public class CacheBackedPeer extends BasePeer
      */
     public static final synchronized void configure( Configuration configuration )
     {
-        enabledAtStartup = configuration.getBoolean("enabledAtStartup");
+        enabledAtStartup = configuration.getBoolean( "enabledAtStartup" );
         enableCacheForCriteriaSelects = configuration.getBoolean( "enableCacheForCriteriaSelects" );
         excludeEnableAtStartUp = configuration.getStringArray( "excludeEnableAtStartUp" );
         excludeEnableCacheForCriteriaSelects = configuration.getStringArray( "excludeEnableCacheForCriteriaSelects" );
-              
-        if ( enabledAtStartup )
+
+        if (enabledAtStartup)
         {
             logger.info( "Cache will be activated (enabledAtStartup=true" );
-            
+
             CacheConfiguration cacheConfiguration = new CacheConfiguration();
             String cacheType = (String) ObjectUtils.defaultIfNull( configuration.getString( "type" ),
                     CacheConfiguration.TYPE_HYBRID );
             String cacheSize = (String) ObjectUtils.defaultIfNull( configuration.getString( "size" ), "10000" );
+            String channelProps = configuration.getString( "multicast.properties" );
             String multicastIP = configuration.getString( "multicast.ip" );
 
-            logger.info( "Initializing cache of type " + cacheType + " with a cache size of " + cacheSize );            
-            
+            logger.info( "Initializing cache of type " + cacheType + " with a cache size of " + cacheSize );
+
             cacheConfiguration.setCacheType( cacheType );
             cacheConfiguration.setLRUCacheSize( cacheSize );
-            if ( multicastIP != null && multicastIP.length() > 0 )
+            if (channelProps != null && channelProps.length() > 0)
+            {
+                logger.info( "Setting channel properties:\t" + channelProps );
+                cacheConfiguration.setChannelProperties( channelProps );
+            }
+            else if (multicastIP != null && multicastIP.length() > 0)
             {
                 logger.info( "Setting multicast IP to " + multicastIP );
                 cacheConfiguration.setMulticastIP( multicastIP );
             }
+
             cacheFactory = new CacheFactory( cacheConfiguration );
-            if ( enableCacheForCriteriaSelects )
+            if (enableCacheForCriteriaSelects)
             {
                 logger.info( "CacheForCriteriaSelects will be activated" );
-            } else
+            }
+            else
             {
-                logger
-                        .info( "CacheForCriteriaSelects will NOT be activated (enableCacheForCriteriaSelects=false or not set" );
+                logger.info( "CacheForCriteriaSelects will NOT be activated (enableCacheForCriteriaSelects=false or not set" );
             }
             logger.info( "Cache initialization completed" );
-        } else
+        }
+        else
         {
             logger.info( "Cache will NOT be activated (enabledAtStartup=false or not set" );
         }
