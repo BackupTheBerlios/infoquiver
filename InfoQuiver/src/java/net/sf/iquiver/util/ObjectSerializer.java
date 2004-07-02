3dc7d45b0db84eea1af04c85edff29a85ee0abdd
netseeker <netseeker>
2004-07-02 20:41:04 +0000

    *** empty log message ***

---
+++ InfoQuiver/src/java/net/sf/iquiver/util/ObjectSerializer.java
@@ -0,0 +1,33 @@
+/*
+ * Created on 02.07.2004
+ */
+package net.sf.iquiver.util;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+
+/**
+ * @author netseeker aka Michael Manske
+ */
+public class ObjectSerializer
+{
+    private static XStream xstream = new XStream(new DomDriver());
+    
+    /**
+     * @param object
+     * @return
+     */
+    public static String objectToXml(Object object)
+    {
+        return xstream.toXML( object );
+    }
+    
+    /**
+     * @param xml
+     * @return
+     */
+    public static Object xmlToObject( String xml )
+    {
+        return xstream.fromXML( xml );
+    }
+}
