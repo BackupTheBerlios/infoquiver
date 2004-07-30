30842b68d890863be34cde7b666f3dedc46340d8
netseeker <netseeker>
2004-07-30 18:40:20 +0000

    *** empty log message ***

--- InfoQuiver/src/java/net/sf/iquiver/service/BaseService.java
+++ InfoQuiver/src/java/net/sf/iquiver/service/BaseService.java
@@ -17,26 +17,26 @@ public abstract class BaseService implements Service
     /**
      * Comment for <code>serviceStateListeners</code>
      */
-    List serviceStateListeners = Collections.synchronizedList(new ArrayList());
-    
+    List serviceStateListeners = Collections.synchronizedList( new ArrayList() );
+
     /**
      * Comment for <code>serviceExecutionListeners</code>
      */
-    List serviceExecutionListeners = Collections.synchronizedList(new ArrayList());
+    List serviceExecutionListeners = Collections.synchronizedList( new ArrayList() );
 
     private int _state = ServiceStateListener.ST_STOPPED;
-    
+
     private int _restartCount = 0;
-    private long _startTime;    
-    
+    private long _startTime;
+
     /* (non-Javadoc)
      * @see net.sf.iquiver.service.Service#addServiceStateListener(net.sf.iquiver.service.ServiceStateListener)
      */
     public final void addServiceStateListener( ServiceStateListener listener )
     {
-        if(!serviceStateListeners.contains(listener))
-        {    
-            serviceStateListeners.add(listener);
+        if (!serviceStateListeners.contains( listener ))
+        {
+            serviceStateListeners.add( listener );
         }
     }
 
@@ -45,7 +45,7 @@ public abstract class BaseService implements Service
      */
     public final void removeServiceStateListener( ServiceStateListener listener )
     {
-        serviceStateListeners.remove(listener);
+        serviceStateListeners.remove( listener );
     }
 
     /* (non-Javadoc)
@@ -53,9 +53,9 @@ public abstract class BaseService implements Service
      */
     public final void addServiceExecutionListener( ServiceExecutionListener listener )
     {
-        if(!serviceExecutionListeners.contains(listener))
-        {    
-            serviceExecutionListeners.add(listener);
+        if (!serviceExecutionListeners.contains( listener ))
+        {
+            serviceExecutionListeners.add( listener );
         }
     }
 
@@ -64,25 +64,26 @@ public abstract class BaseService implements Service
      */
     public final void removeServiceExecutionListener( ServiceExecutionListener listener )
     {
-        serviceExecutionListeners.remove(listener);
+        serviceExecutionListeners.remove( listener );
     }
-    
+
     /**
      * The recommended method for all inherited classes to notify ServiceStateListeners
      * of a ServiceStateChangedEvent.
      * @param evt
      */
-    protected final void notifyServiceStateListeners(final ServiceStateChangedEvent evt)
+    protected final void notifyServiceStateListeners( final ServiceStateChangedEvent evt )
     {
-        new Thread() {
+        new Thread()
+        {
             public void run()
             {
-                for(Iterator it = serviceStateListeners.iterator(); it.hasNext(); )
+                for (Iterator it = serviceStateListeners.iterator(); it.hasNext();)
                 {
-                    ((ServiceStateListener)it.next()).serviceStateChanged(evt);
+                    ((ServiceStateListener) it.next()).serviceStateChanged( evt );
                 }
             }
-        }.start();        
+        }.start();
     }
 
     /**
@@ -90,19 +91,20 @@ public abstract class BaseService implements Service
      * of a ServiceExecutionEvent.
      * @param evt
      */
-    protected final void notifyServiceExecutionListeners(final ServiceExecutionEvent evt)
+    protected final void notifyServiceExecutionListeners( final ServiceExecutionEvent evt )
     {
-        new Thread() {
+        new Thread()
+        {
             public void run()
             {
-                for(Iterator it = serviceExecutionListeners.iterator(); it.hasNext(); )
+                for (Iterator it = serviceExecutionListeners.iterator(); it.hasNext();)
                 {
-                    ((ServiceExecutionListener)it.next()).executed(evt);
+                    ((ServiceExecutionListener) it.next()).executed( evt );
                 }
             }
         }.start();
     }
-    
+
     /**
      * The recommended method for all inherited classes to execute all neccessary actions/notifications
      * if the service state changes 
@@ -113,12 +115,12 @@ public abstract class BaseService implements Service
     {
         int oldState = this._state;
         this._state = newState;
-        
-        notifyServiceStateListeners(new ServiceStateChangedEvent(this, oldState, newState));
-        
+
+        notifyServiceStateListeners( new ServiceStateChangedEvent( this, oldState, newState ) );
+
         return oldState;
-    }        
-    
+    }
+
     /* (non-Javadoc)
      * @see net.sf.iquiver.service.Service#getState()
      */
@@ -126,26 +128,27 @@ public abstract class BaseService implements Service
     {
         return this._state;
     }
-    
+
     /* (non-Javadoc)
      * @see org.apache.avalon.framework.activity.Startable#start()
      */
     public void start()
     {
         this._startTime = System.currentTimeMillis();
-        this._restartCount++;        
+        this._restartCount++;
         doStart();
-        setState(ServiceStateListener.ST_STARTED);
+        setState( ServiceStateListener.ST_STARTED );
     }
+
     /* (non-Javadoc)
      * @see org.apache.avalon.framework.activity.Startable#stop()
      */
     public void stop()
     {
         doStop();
-        setState(ServiceStateListener.ST_STOPPED);
+        setState( ServiceStateListener.ST_STOPPED );
     }
-    
+
     /* (non-Javadoc)
      * @see net.sf.iquiver.service.Service#getStartTime()
      */
@@ -161,7 +164,8 @@ public abstract class BaseService implements Service
     {
         return _restartCount;
     }
-    
+
     protected abstract void doStart();
-    protected abstract void doStop();    
+
+    protected abstract void doStop();
 }
