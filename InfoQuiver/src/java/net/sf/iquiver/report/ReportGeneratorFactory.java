48b597095e3a780bf96cffbadf737a1cc2c8db50
netseeker <netseeker>
2004-10-24 16:32:58 +0000

    switched default report generator from jasper to our own report generation engine

--- InfoQuiver/src/java/net/sf/iquiver/report/ReportGeneratorFactory.java
+++ InfoQuiver/src/java/net/sf/iquiver/report/ReportGeneratorFactory.java
@@ -2,18 +2,19 @@
  * ReportGeneratorFactory.java
  * created on 24.10.2004 by netseeker
  * $Source: /home/xubuntu/berlios_backup/github/tmp-cvs/infoquiver/Repository/InfoQuiver/src/java/net/sf/iquiver/report/ReportGeneratorFactory.java,v $
- * $Date: 2004/10/24 16:27:55 $
- * $Revision: 1.1 $
+ * $Date: 2004/10/24 16:32:58 $
+ * $Revision: 1.2 $
  *********************************************************************/
 
 package net.sf.iquiver.report;
+
+import net.sf.iquiver.IQuiver;
+import net.sf.iquiver.report.impl.DefaultReportGenerator;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import de.netseeker.util.StringUtil;
-import net.sf.iquiver.IQuiver;
-import net.sf.iquiver.report.impl.JasperReportGenerator;
 
 /**
  * @author netseeker aka Michael Manske
@@ -36,7 +37,7 @@ public class ReportGeneratorFactory
         if (generator == null)
         {
             String clazz = StringUtil.defaultIfNullOrEmpty( IQuiver.getConfiguration().getString(
-                    CONFIG_REPORT_GENERATOR ), JasperReportGenerator.class.getName() );
+                    CONFIG_REPORT_GENERATOR ), DefaultReportGenerator.class.getName() );
             try
             {
                 generator = (ReportGenerator) Class.forName( clazz ).newInstance();
