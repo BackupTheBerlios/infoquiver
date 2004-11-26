e67777da02d4a2ee4539c0f6bb5170aded846097
netseeker <netseeker>
2004-11-26 22:51:55 +0000

    added automatic mail dispatching of generated reports

--- InfoQuiver/src/java/net/sf/iquiver/service/impl/ContentSearchService.java
+++ InfoQuiver/src/java/net/sf/iquiver/service/impl/ContentSearchService.java
@@ -2,8 +2,8 @@
  * ContentSearchService.java
  * created on 15.07.2004 by netseeker
  * $Source: /home/xubuntu/berlios_backup/github/tmp-cvs/infoquiver/Repository/InfoQuiver/src/java/net/sf/iquiver/service/impl/ContentSearchService.java,v $
- * $Date: 2004/11/26 19:26:44 $
- * $Revision: 1.13 $
+ * $Date: 2004/11/26 22:51:55 $
+ * $Revision: 1.14 $
  *********************************************************************/
 
 package net.sf.iquiver.service.impl;
@@ -20,10 +20,11 @@ import net.sf.iquiver.om.ContentSource;
 import net.sf.iquiver.om.SearchQuery;
 import net.sf.iquiver.om.User;
 import net.sf.iquiver.om.UserGroup;
-import net.sf.iquiver.om.UserGroupLnkPeer;
+import net.sf.iquiver.om.UserGroupPeer;
 import net.sf.iquiver.om.UserPeer;
 import net.sf.iquiver.report.IReportSource;
 import net.sf.iquiver.report.ReportGeneratorFactory;
+import net.sf.iquiver.report.impl.ReportDispatcher;
 import net.sf.iquiver.search.QueryTask;
 import net.sf.iquiver.service.BaseService;
 
@@ -31,13 +32,12 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.lucene.queryParser.ParseException;
 import org.apache.torque.TorqueException;
-import org.apache.torque.util.Criteria;
 
 import de.netseeker.util.ArrayMap;
 import de.netseeker.util.ListMap;
 
 /**
- * This is one of InfoQuivers main services. It starts searches for all clients, usergroups and users
+ * This is one of InfoQuivers core services. It starts searches for all clients, usergroups and users
  * according to their search parameters. This service invokes report generation for all search results. 
  * @author netseeker aka Michael Manske
  */
@@ -57,30 +57,29 @@ public class ContentSearchService extends BaseService
     {
         try
         {
-            List clients = ClientPeer.doSelectAll();
-
-            for (Iterator it = clients.iterator(); it.hasNext();)
+            List reportsources = ClientPeer.doSelectAll();
+            for (Iterator it = reportsources.iterator(); it.hasNext();)
             {
                 Client client = (Client) it.next();
                 createSearchReports( client );
-
-                List groups = client.getUserGroups();
-
-                for (Iterator gIt = groups.iterator(); gIt.hasNext();)
+            }
+            
+            reportsources = UserGroupPeer.doSelectAll();
+            for (Iterator gIt = reportsources.iterator(); gIt.hasNext();)
+            {
+                UserGroup group = (UserGroup) gIt.next();
+                createSearchReports( group );
+            }
+            
+            reportsources = UserPeer.doSelectAll();
+            for (Iterator uIt = reportsources.iterator(); uIt.hasNext();)
+            {
+                User user = (User) uIt.next();
+                if (!user.getSearchQuerys().isEmpty())
                 {
-                    UserGroup group = (UserGroup) gIt.next();
-                    createSearchReports( group );
-
-                    Criteria crit = new Criteria();
-                    crit.addJoin( UserPeer.USER_ID, UserGroupLnkPeer.USER_ID );
-                    crit.add( UserGroupLnkPeer.USER_GROUP_ID, group.getUserGroupId() );
-                    List users = UserPeer.doSelect( crit );
-
-                    for (Iterator uIt = users.iterator(); uIt.hasNext();)
-                    {
-                        User user = (User) uIt.next();
-                        createSearchReports( user );
-                    }
+                    createSearchReports( user );
+                    ReportDispatcher dispatcher = new ReportDispatcher( user );
+                    dispatcher.start();
                 }
             }
         }
@@ -107,51 +106,47 @@ public class ContentSearchService extends BaseService
     private void createSearchReports( IReportSource obj ) throws TorqueException
     {
         List queries = obj.getSearchQuerys();
-        
-        if (!queries.isEmpty())
+        List contentSources = obj.getContentSources();
+        List documents = null;
+        ListMap queryResults = new ArrayMap();
+        String[] tmp, indeces;
+
+        // determine all indeces to use for searching, one index per content source
+        tmp = new String[contentSources.size()];
+        int count = 0;
+        for (int i = 0; i < contentSources.size(); i++)
         {
-            List contentSources = obj.getContentSources();
-            List documents = null;
-            ListMap queryResults = new ArrayMap();
-            String[] tmp, indeces;
-
-            // determine all indeces to use for searching, one index per content source
-            tmp = new String[contentSources.size()];
-            int count = 0;
-            for (int i = 0; i < contentSources.size(); i++)
+            ContentSource source = (ContentSource) contentSources.get( i );
+            String index = _indexDirectory + File.separator + String.valueOf( obj.getClientId() );
+            File testFile = new File( index );
+            if (testFile.exists())
             {
-                ContentSource source = (ContentSource) contentSources.get( i );
-                String index = _indexDirectory + File.separator + String.valueOf( source.getContentSourceId() );
-                File testFile = new File( index );
-                if (testFile.exists())
-                {
-                    tmp[i] = index;
-                    count++;
-                }
+                tmp[i] = index;
+                count++;
             }
-
-            indeces = new String[count];
-            System.arraycopy( tmp, 0, indeces, 0, count );
-
-            for (Iterator it = queries.iterator(); it.hasNext();)
-            {
-                SearchQuery query = (SearchQuery) it.next();
-                try
-                {
-                    documents = QueryTask.search( indeces, query.getSearchQueryCriteria() );
-                    queryResults.put( query, documents );
-                }
-                catch ( IOException e )
-                {
-                    logger.error( e );
-                }
-                catch ( ParseException e )
-                {
-                    logger.error( e );
-                }
-            }
-
-            ReportGeneratorFactory.getReportGenerator().generate( obj, queryResults );
         }
+
+        indeces = new String[count];
+        System.arraycopy( tmp, 0, indeces, 0, count );
+
+        for (Iterator it = queries.iterator(); it.hasNext();)
+        {
+            SearchQuery query = (SearchQuery) it.next();
+            try
+            {
+                documents = QueryTask.search( indeces, query.getSearchQueryCriteria() );
+                queryResults.put( query, documents );
+            }
+            catch ( IOException e )
+            {
+                logger.error( e );
+            }
+            catch ( ParseException e )
+            {
+                logger.error( e );
+            }
+        }
+
+        ReportGeneratorFactory.getReportGenerator().generate( obj, queryResults );
     }
 }
